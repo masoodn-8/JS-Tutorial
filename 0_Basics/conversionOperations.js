4530888981e22ad8e0bf3105fc7758bedaa9326f
masoodn-8 <141218065+masoodn-8@users.noreply.github.com>
2025-02-21 18:37:42 +0000

    Conversion Operations in JS

---
+++ 0_Basics/conversionOperations.js
@@ -0,0 +1,35 @@
+// If we want to convert one dataType into another dataType we will use TypeCasting conversion
+
+let id = 2202; // we have declared ID in number.
+
+// Now if we want to convert that into String we will use type conversion.
+
+let strId = String (id); // This will convert number into String.
+
+console.log(strId);
+console.log(typeof strId);
+
+let numId = Number (strId);
+
+console.log(typeof numId); // This will convert string into number.
+let bool = true;
+
+let strBool = String (bool) //This will boolean into String
+console.log(strBool);
+
+let Boolstr = "True"
+
+let strB = Boolean(Boolstr); //This will convert String to Boolean
+
+console.log(strB);
+
+
+
+/* Conversion Types : 
+     
+   number => String
+   String => number & boolean
+   Boolean => String & number
+
+*/
+
